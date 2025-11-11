e381862a65d90031ed36c9187d317ab7066d5894
Alperen Demirdöğer <alperendemirdoger@gmail.com>
2025-11-11 23:49:08 +0300

    Add Nasıl Çalışır page with two sections and CTA

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero'
+import NasilCalisir from './pages/NasilCalisir'
 
 function App() {
   return (
     <div className="min-h-screen">
       <Hero />
+      <NasilCalisir />
     </div>
   )
 }
